bdce3ecf203b94d25baecf514ab36a93acf855cb
Vitalijus Desukas <vitalij.desuk@gmail.com>
2025-02-20 12:44:23 +0200

    fix gitGuardian complain

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '38ada706c8b6910b07f53012d586a0210c0306032a192b016f6085038ff2a76c2ebaca7254639afbf37b3e83a3ee901360213abfbb7c4bae1060bd8b21ee9cd0'
+  # config.secret_key = ''
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
